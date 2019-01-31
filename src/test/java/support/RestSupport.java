2f1452203b0452ef0ec7c012dd0e47f806440e18
Davies, Christopher <christopher.davies2@sky.uk>
2019-01-31 13:30:39 +0000

    Added log4j to repo and logging for a basic GET request.

--- src/test/java/support/RestSupport.java
+++ src/test/java/support/RestSupport.java
@@ -6,8 +6,14 @@ import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
 import java.util.Map;
+import org.apache.log4j.Logger;
 
 public class RestSupport {
+
+    private final static Logger logger = Logger.getLogger(RestSupport.class);
+    private static final String LINE_BREAK = "---------------------------------------------------------";
+    private static final String REQUEST = "REQUEST";
+
     private RequestSpecification request = RestAssured.given();
     private String baseUri;
 
@@ -16,7 +22,11 @@ public class RestSupport {
     }
 
     public Response get(String path) {
-        return request.get(baseUri + path);
+        // TODO: need ability to add header to the request and log them
+        String endpoint = baseUri + path;
+        Response response = request.get(endpoint);
+        logRequestDetails(endpoint);
+        return response;
     }
 
     public Response post(String path, DataTable table) {
@@ -35,5 +45,12 @@ public class RestSupport {
 
     private Map<String,String> convertDataTableToMap(DataTable table) {
         return table.asMap(String.class, String.class);
+    }
+
+    private void logRequestDetails(String endpoint) {
+        logger.info(LINE_BREAK);
+        logger.info(REQUEST);
+        logger.info(String.format("GET %s", endpoint));
+        logger.info(LINE_BREAK);
     }
 }
